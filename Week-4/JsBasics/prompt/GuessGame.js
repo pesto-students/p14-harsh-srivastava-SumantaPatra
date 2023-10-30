0a5848ad0ff0b9efb5565b77005a5e92fbf2dbc4
sumanta patra <sumanta@sumantas-MacBook-Pro.local>
2023-10-30 21:25:43 +0530

    add playGuessingGame using user prompt

---
+++ Week-4/JsBasics/prompt/GuessGame.js
@@ -0,0 +1,16 @@
+const prompt = require("prompt-sync")();
+function playGuessingGame(numToGuess,totalGuesses=10){
+    let defaultMessage = "Enter a number between 1 and 1000"
+    for(let i=0; i<totalGuesses; i++){
+        const inputVal = prompt(defaultMessage);
+        if(inputVal === null) return 0;
+        const inputNumber = parseInt(inputVal);
+        if(isNaN(inputNumber)) defaultMessage = "Please enter a number."
+        else if(inputNumber < numToGuess) defaultMessage = "Number is too small. Guess a larger number";
+        else if(inputNumber > numToGuess) defaultMessage = "Number is too large. Guess a smaller number."
+        else return i+1;
+    }
+    return 0;
+}
+
+console.log(playGuessingGame(1,4))
